77e40d34aa04b3c570c6f2233cf2472a0bb47221
shanshan <sslee666999@gmail.com>
2022-10-29 13:52:29 -0400

    update the header

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter} from 'react-router-dom';
 
 import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar/Sidebar';
 
 const App = () => {
     
@@ -13,14 +12,6 @@ const App = () => {
                     {/* below is Header */}
                     <div>
                         <Header />
-                    </div>
-                    {/* below is main page */}
-                        <Sidebar/>
-                    {/* below is footer */}
-                    <div className="mt-24">
-                        <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-                            © 2022 All rights reserved by RevolutionArt.com
-                        </p>
                     </div>
                 </div>
             </BrowserRouter>
