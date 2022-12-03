580ef5134c958a7a4bcc90bd3693db4f96c81c4b
sslee <91924792+sslcandoit@users.noreply.github.com>
2022-12-02 20:45:17 -0500

    Delete server directory

--- server/models/user.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  id: { type: String },
-});
-
-export default mongoose.model("User", userSchema);
