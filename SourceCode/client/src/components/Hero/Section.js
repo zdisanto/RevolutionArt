f729a35b091f8aab0f16b668864d35cdbea9bbce
Kishore Kanchan <96896065+kishore1521@users.noreply.github.com>
2023-02-23 19:36:56 -0500

    Update Section.js

--- SourceCode/client/src/components/Hero/Section.js
+++ SourceCode/client/src/components/Hero/Section.js
@@ -13,7 +13,7 @@ function Section() {
         <h2 className='section-heading'>Revolutionizing The Way World Views <span className='art-span-section'>Art</span></h2>
          <h6 className='title-two'>Find art works from the most popular artisits of this year.</h6>
          <button className="bg-orange-500 hover:bg-gray-700 text-white  py-2 px-10 border border-white-700 rounded">
-  Browse Mores
+  Browse More
 </button>
       </div>
     </div>
